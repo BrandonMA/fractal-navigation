ba944e32bcf174342a6bc649487bb509aedebe22
BrandonMA <brandonma98@protonmail.com>
2021-01-12 18:49:46 -0600

    updated build

--- dist/components/AnimatedRoute/AnimatedRoute.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import { RouteProps } from '../../react-router';
-interface AnimatedRouteProps extends Omit<RouteProps, 'children'> {
-    children: JSX.Element;
-}
-export declare function AnimatedRoute({ children, ...others }: AnimatedRouteProps): JSX.Element;
-export {};
