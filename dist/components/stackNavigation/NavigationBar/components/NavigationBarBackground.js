4557a1007093278c173587bfefa41026638a6964
BrandonMA <brandonma98@protonmail.com>
2021-01-25 15:24:13 -0600

    Shadow properties are now editable for the navigation bar, so we can provide a transparent bar without affecting the buttons

--- dist/components/stackNavigation/NavigationBar/components/NavigationBarBackground.js
+++ dist/components/stackNavigation/NavigationBar/components/NavigationBarBackground.js
@@ -4,6 +4,6 @@ import { BaseBox } from '@bma98/fractal-ui';
 export var NavigationBarBackground = memo(function (_a) {
     var children = _a.children;
     var _b = useTheme(), navigationBar = _b.navigationBar, shadowProperties = _b.shadowProperties;
-    return (React.createElement(BaseBox, { backgroundColor: 'navigationBarBackground', flexDirection: 'row', height: navigationBar.navigationBarHeight, shadowOffset: shadowProperties.offset, shadowColor: 'shadowColor', shadowOpacity: shadowProperties.opacity, shadowRadius: shadowProperties.radius, width: '100%', zIndex: 2000, paddingHorizontal: 'm', elevation: 1 }, children));
+    return (React.createElement(BaseBox, { backgroundColor: 'navigationBarBackground', flexDirection: 'row', height: navigationBar.navigationBarHeight, shadowOffset: shadowProperties.navigationBarOffset, shadowColor: 'shadowColor', shadowOpacity: shadowProperties.navigationBarOpacity, shadowRadius: shadowProperties.navigationBarRadius, width: '100%', zIndex: 2000, paddingHorizontal: 'm', elevation: 1 }, children));
 });
 //# sourceMappingURL=NavigationBarBackground.js.map
